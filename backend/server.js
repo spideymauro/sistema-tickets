b3b2427d55c49ffb723942f0fc5525be7cf307d1
spideymauro <104883094+spideymauro@users.noreply.github.com>
2026-09-06 23:51:11 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -50,6 +50,6 @@ app.use((err, req, res, next) => {
   next();
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor backend corriendo en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor backend corriendo en el puerto ${PORT}`);
 });
